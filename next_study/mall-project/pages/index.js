60e7a7bf44c9380e06a97a595c648f3840f0209f
DESKTOP-CS6T6U3\ELUO <jh.yoo@eluocnc.com>
2024-06-24 13:40:53 +0900

    next 스터디

---
+++ next_study/mall-project/pages/index.js
@@ -0,0 +1,30 @@
+import ProductList from '@/components/ProductList';
+import SearchForm from '@/components/SearchForm';
+import axios from '@/lib/axios';
+import styles from '@/styles/Home.module.css';
+import Head from 'next/head';
+
+export async function getStaticProps() {
+
+  const res = await axios.get('/products');
+  const products = res.data.results;
+  
+  return {
+    props: {
+      products,
+    }
+  }
+}
+
+export default function Home({ products }) {
+
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+      </Head>
+      <SearchForm />
+      <ProductList className={styles.products} products={products} />
+    </>
+  )
+}
